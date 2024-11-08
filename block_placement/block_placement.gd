dca332b3bd98bd8e9301b0b31f0a167ca1328694
Bogdan <bogdan.jocic@pixels2pixels.com>
2024-11-08 16:11:44 +0100

    added block placement

---
+++ block_placement/block_placement.gd
@@ -0,0 +1,78 @@
+extends Spatial
+
+
+enum Mode {
+	PLACE,
+	DELETE
+}
+
+onready var preview_block: MeshInstance = $PreviewBlock
+
+var mode: int = Mode.PLACE
+var gridmap_position: Vector3 = Vector3()
+var gridmap: GridMap
+
+func _ready():
+	ControllerManager.block_placement = self
+
+
+func set_mode(new_mode: int):
+	mode = new_mode
+	match mode:
+		Mode.PLACE:
+			preview_block.mesh.material.emissiom = Color.aqua
+		Mode.DELETE:
+			preview_block.mesh.material.emissiom = Color.red
+
+
+func _unhandled_input(event):
+	if event is InputEventScreenDrag:
+		process_drag(event)
+	elif event is InputEventScreenTouch:
+		if event.pressed:
+			process_drag(event)
+		else:
+			_on_touch_released()
+			preview_block.visible = false
+
+
+func _on_touch_released():
+	if not preview_block.visible: return
+	if mode == Mode.PLACE:
+		gridmap.set_cell_item(gridmap_position.x, gridmap_position.y, gridmap_position.z, 0)
+	elif mode == Mode.DELETE:
+		gridmap.set_cell_item(gridmap_position.x, gridmap_position.y, gridmap_position.z, -1)
+
+
+func process_drag(event: InputEvent):
+	var camera: Camera = get_viewport().get_camera()
+	var space_state: PhysicsDirectSpaceState = get_world().direct_space_state
+	var from: Vector3 = camera.project_ray_origin(event.position)
+	var to: Vector3 = from + camera.project_ray_normal(event.position)*100000
+	var result: Dictionary = space_state.intersect_ray(from, to, [], 1)
+	print(result)
+	if result.size() == 0:
+		preview_block.visible = false
+	else:
+		var collider: Node = result["collider"]
+		if collider is GridMap:
+			gridmap = collider
+			var world_coordinates: Vector3 = result["position"]
+			var normal: Vector3 = normalize_normal(result["normal"])
+			var gridmap_coordinates: Vector3 = collider.world_to_map(world_coordinates - result["normal"]*0.1)
+			if normal != Vector3():
+				preview_block.global_position = gridmap_coordinates + Vector3.ONE*0.5 + normal
+				gridmap_position = gridmap_coordinates + (normal if mode == Mode.PLACE else Vector3()) 
+			preview_block.visible = true
+		else:
+			preview_block.visible = false
+
+
+func normalize_normal(normal: Vector3):
+	if normal.x > 0.95: return Vector3(1,0,0)
+	if normal.y > 0.95: return Vector3(0,1,0)
+	if normal.z > 0.95: return Vector3(0,0,1)
+	if normal.x < -0.95: return Vector3(-1,0,0)
+	if normal.y < -0.95: return Vector3(0,-1,0)
+	if normal.z < -0.95: return Vector3(0,0,-1)
+	return Vector3()
--- controller/controller_manager.gd
+++ controller/controller_manager.gd
@@ -4,6 +4,7 @@ extends Node
 var movement_joystick: Control
 var camera_joystick: Control
 var player_toggled: bool = true
+var block_placement
 
 func toggle_player():
 	player_toggled = not player_toggled
