f7baeb4a2224ba623bea231838842fc268e62c72
Bogdan <bogdan.jocic@pixels2pixels.com>
2024-11-08 18:04:02 +0100

    trying to fix joystick multitouch

--- controller/joystick/joystick.gd
+++ controller/joystick/joystick.gd
@@ -11,18 +11,18 @@ var touch_index: int = -1
 func _unhandled_input(event):
 	var radius = (border.rect_size.x*rect_scale.x)/2.0
 	if event is InputEventScreenTouch:
-		if event.pressed and get_global_mouse_position().distance_to(rect_global_position) < radius:
-			accept_event()
-			process_drag(event, radius)
+		var inside: bool = get_global_mouse_position().distance_to(rect_global_position) < radius
+		if not event.pressed:
+			if touch_index == event.index:
+				touch_index = -1
+				stick.rect_position = Vector2()
+				input = Vector2()
+		elif inside:
 			touch_index = event.index
-		elif not event.pressed and touch_index == event.index:
-			touch_index = -1
-			stick.rect_position = Vector2()
-			input = Vector2()
+			process_drag(event, radius)
 	elif event is InputEventScreenDrag:
 		if touch_index == event.index:
 			process_drag(event, radius)
-			accept_event()
 
 
 func process_drag(event, radius):
@@ -31,4 +31,5 @@ func process_drag(event, radius):
 	stick.rect_position = diff*3.0
 	input = diff/radius
 	input.y *= -1
+	accept_event()
 	
