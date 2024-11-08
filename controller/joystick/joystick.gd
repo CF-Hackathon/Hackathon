6edf94ac929f872193af541b432af88124635e02
Bogdan <bogdan.jocic@pixels2pixels.com>
2024-11-08 14:51:01 +0100

    added joystick

---
+++ controller/joystick/joystick.gd
@@ -0,0 +1,31 @@
+extends Control
+
+
+onready var border: TextureRect = $Border
+onready var stick: Control = $Border/Control/Stick
+
+var input: Vector2
+var touch_index: int = -1
+
+func _unhandled_input(event):
+	var radius = (border.rect_size.x*rect_scale.x)/2.0
+	if event is InputEventScreenTouch:
+		if event.pressed and get_global_mouse_position().distance_to(rect_global_position) < radius:
+			accept_event()
+			process_drag(event, radius)
+			touch_index = event.index
+		elif not event.pressed and touch_index == event.index:
+			touch_index = -1
+			stick.rect_position = Vector2()
+	elif event is InputEventScreenDrag:
+		if touch_index == event.index:
+			process_drag(event, radius)
+			accept_event()
+
+
+func process_drag(event, radius):
+	var stick_position: Vector2 = event.position
+	var diff: Vector2 = event.position - rect_global_position
+	stick.rect_position = diff.clamped(radius)
+	input = diff/radius
+	
