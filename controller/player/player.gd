f065316f1a3f45f9952a036d935645104c757375
Ivan Kocic <129184398+IvanKocic-dev@users.noreply.github.com>
2024-11-09 18:28:33 +0100

    cells behaviors added

--- controller/player/player.gd
+++ controller/player/player.gd
@@ -8,11 +8,12 @@ onready var camera: Camera = $Camera
 var velocity: Vector3
 
 var gravity: float = 10.0
+var default_movement_speed: float = 5.0
 var movement_speed: float = 5.0
 var jump_height: float = 7.0
 var sensitivity: float = 2.0
 var portal_cooldown: float = 0.0
-
+var health: float = 100.0
 
 func _ready():
 	ControllerManager.player = self
@@ -56,10 +57,29 @@ func detect_cell_collisions():
 		var tp = grid_map.world_to_map(coll.position - coll.normal*0.5)
 		var block_id = grid_map.get_cell_item(tp.x, tp.y, tp.z)
 		if block_id == -1: continue
-		var block_name = mesh_lib.get_item_name(block_id)
-		if block_name == "Portal" and portal_cooldown <= 0:
-			get_tree().paused = true
-			portal_cooldown = 2
-			ControllerManager.world_browser.popup_centered()
-		#print(block_id)
-	#grid_map.mesh_library.get
+		
+		match block_id:
+			CellNames.GRASS:
+				self.movement_speed = default_movement_speed 
+			CellNames.DIRT:
+				self.movement_speed = default_movement_speed - 2.0
+			CellNames.LAVA:
+				reduce_health(0.1)
+			CellNames.GLASS:
+				pass
+			CellNames.ICE:
+				self.movement_speed = default_movement_speed + 2.0
+			CellNames.PORTAL:
+				if portal_cooldown <= 0:
+					get_tree().paused = true
+					portal_cooldown = 2
+					ControllerManager.world_browser.popup_centered()
+					
+
+func add_health(amount: float):
+	self.health += amount
+	if self.health > 100.0: self.health = 100.0
+
+func reduce_health(amount: float):
+	self.health -= amount
+	if self.health < 0: self.health = 0
